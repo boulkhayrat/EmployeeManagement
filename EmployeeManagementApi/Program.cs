a29848d8158cf17bb44e3d297ea7bc445120aeef
Abdelilah BOULKHAYRAT <abdelilah.boulkhayrat@kazyon.com>
2024-06-28 00:08:37 +0100

    Initial commit

---
+++ EmployeeManagementApi/Program.cs
@@ -0,0 +1,38 @@
+using EmployeeManagementAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+// Add CORS configuration
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins",
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+                   .AllowAnyMethod()
+                   .AllowAnyHeader();
+        });
+});
+
+builder.Services.AddDbContext<EmployeeContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+// Use CORS
+app.UseCors("AllowAllOrigins");
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
